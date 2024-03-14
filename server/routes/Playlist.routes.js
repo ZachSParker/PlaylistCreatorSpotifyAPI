9393c0a2c6cda3d4a7a9eb49b3b8f70b33ee83ac
Zach Parker <evergreenzach@gmail.com>
2024-03-14 03:00:03 -0600

    updated playlist.routes import statement

--- server/routes/Playlist.routes.js
+++ server/routes/Playlist.routes.js
@@ -1,11 +1,12 @@
 import {Router} from 'express'
-import { createBook,getAllBooks,getOneBook,updateOneBook,deleteOneBook} from '../controllers/book.controller.js';
+import { createPlaylist,getAllPlaylists,getOnePlaylist,updateOnePlaylist,deleteOnePlaylist} from '../controllers/book.controller.js';
+
 const router = Router();
 
 router.route('/playlist/:id')
     .get(getOnePlaylist) //get one book
     .put(updateOnePlaylist) //update a book
-    .delete(deletePlaylist); // delete a book
+    .delete(deleteOnePlaylist); // delete a book
 
 router.route('/playlists')
     .get(getAllPlaylists)
