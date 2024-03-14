7d9ac4ad9babae502c9baf54e36aec5ef4583984
Zach Parker <evergreenzach@gmail.com>
2024-03-13 21:53:09 -0600

    added searchbar and api function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,31 @@
-import { useState,useEffect } from 'react'
+import React from 'react'
 import {
   BrowserRouter,
   Routes,
   Route} 
   from 'react-router-dom';
-import AuthToken from '../src/tokens/AuthToken.js';
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+  import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Dashboard from './views/Dashboard';
+import CreatePlaylist from './views/CreatePlaylist';
+import OnePlaylist from './views/OnePlaylist';
+import UpdatePlaylist from './views/UpdatePlaylist';
 
 function App() {
   
-  const logoutHandler = (e) =>{
-    e.preventDefault()
-    window.localStorage.removeItem("token")
-    setToken("")
-
-  }
+  
   return (
     <>
-      <div>
-        <h1>Spotify Playlist Creator</h1>
-        {!token ?
-          <a href={`${spotifyURL.AUTH_ENDPOINT}?client_id=${spotifyURL.CLIENT_ID}&redirect_uri=${spotifyURL.REDIRECT_URI}&response_type=${spotifyURL.RESPONSE_TYPE}`}>Login to Spotify</a>
-          : <button onClick={logoutHandler}>Logout from Spotify</button>}
-      </div>
+
+      <BrowserRouter>
+        <Header/>
+        <Routes basename="/playlist">
+          <Route path="" element={<Dashboard/>} />
+          <Route path="/new" element={<CreatePlaylist/>}/>
+          <Route path="/:id/details" element={<OnePlaylist/>} />
+          <Route path="/:id/edit" element={<UpdatePlaylist/>}/>
+        </Routes>
+      </BrowserRouter>
       
     </>
   )
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
-
+import {useState,useEffect,React} from 'react'
+import dotenv from 'dotenv';
+import axios from 'axios';
 const Header = () => {
+  const [spotifyURL,setSpotifyURL] = useState({
+    CLIENT_ID : "1a2a1a5f4bb745928d1de9e85e80d737",
+    REDIRECT_URI:"http://localhost:5173",
+    AUTH_ENDPOINT:"https://accounts.spotify.com/authorize",
+    RESPONSE_TYPE:"token"
+  })
+  const [searchKey,setSearchKey] = useState("");
+  const [token,setToken] = useState("")
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+    // console.log(spotifyURL.CLIENT_ID);
+    if(!token && hash){
+      token = hash.substring(1).split("&").find(element => element.startsWith("access_token")).split("=")[1]
+      
+      window.location.hash = ""
+      window.localStorage.setItem("token",token)
+      
+    }
+    setToken(token);
+  },[token])
+
+  const logoutHandler = (e) =>{
+    e.preventDefault()
+    window.localStorage.removeItem("token")
+    setToken("")
+
+  }
+  const artistSearch = async (e) =>{
+    e.preventDefault();
+    const{data} = await axios.get("https://api.spotify.com/v1/search",{
+      headers:{
+        Authorization:`Bearer ${token}`
+      },
+      params:{
+        q: searchKey,
+        type:"artist",
+        type:"track",
+        limit:5
+        
+
+      }
+      
+    })
+    console.log(data)
+
+  }
   return (
-    <div>Spotify Header</div>
+    <div>
+        <h1>Spotify Playlist Creator</h1>
+        {!token ?
+          <a href={`${spotifyURL.AUTH_ENDPOINT}?client_id=${spotifyURL.CLIENT_ID}&redirect_uri=${spotifyURL.REDIRECT_URI}&response_type=${spotifyURL.RESPONSE_TYPE}`}>Login to Spotify</a>
+          : <button onClick={logoutHandler}>Logout from Spotify</button>}
+        {token ?
+        <form onSubmit={artistSearch}>
+          <input type="text" placeholder='search for artists/songs' onChange={e=> setSearchKey(e.target.value)}/>
+          <button type="submit">Search</button>
+        </form>
+        :<h5>Please Login</h5>
+        }
+      </div>
+      
   )
 }
 
