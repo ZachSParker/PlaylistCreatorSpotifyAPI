d7046c99fb0d91398e29b1e5f88a7e5b9ecabeaf
Zach Parker <evergreenzach@gmail.com>
2024-03-14 02:51:38 -0600

    added playlist model document

---
+++ server/models/Playlist.model.js
@@ -0,0 +1,30 @@
+import {model, Schema} from 'mongoose';
+const PlaylistSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: [true, "Title is required!"],
+            minlength: [2, "Title must contain 2 characters or more!"],
+            maxlength: [255, "Title needs to be less than 255 characters long"]
+        },
+        genre: {
+            type: String,
+            required: [true, "genre is required!"],
+            minlength: [5, "genre must be at least 5 characters long!"],
+            maxlength: [255, "genre needs to be less than 255 characters long"]
+        },
+        description: {
+            type: String,
+            required: [true, "description of your playlist is required!"],
+            minlength: [5, "description must be at least 5 characters long!"],
+            maxlength: [255, "description needs to be less than 255 characters long"]
+        },
+        songs:{
+            type: Array,
+            required:[true,"at least 1 song is required!"],
+        }
+    },
+    { timestamps: true }
+);
+const Playlist = model("Playlist", PlaylistSchema);
+export default Playlist;
